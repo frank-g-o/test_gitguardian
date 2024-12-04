47b57dd5cb605d7c478147fb1b0eb94762cdcffe
frank-g-o <85952263+frank-g-o@users.noreply.github.com>
2024-12-04 11:43:34 +0100

    Update bucket_s3.py
    testing out

--- bucket_s3.py
+++ bucket_s3.py
@@ -5,6 +5,10 @@ from typing import Dict
 import aws_lib
 import pymongo
 
+def aws_upload(data: Dict):
+    database = aws_lib.connect("<template_var>", "<template_secret>")
+    database.push(data)
+    
 
 def aws_upload(data: Dict):
     database = aws_lib.connect("AKIAF6BAFJKR45SAWSZ5", "hjshnk5ex5u34565AWS654/JKGjhz545d89sjkja")
